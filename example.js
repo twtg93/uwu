c5a2d7a5c3e9415143e94b53569b19ac5253c3f9
twtg93 <82419332+twtg93@users.noreply.github.com>
2022-02-06 10:33:40 -0600

    Create example.js

---
+++ example.js
@@ -0,0 +1,5 @@
+import Uwu from 'https://raw.githubusercontent.com/twtg93/uwu/main/uwu.js';
+
+const UwU = new Uwu();
+
+document.write(UwU.uwuifySentence('Hey there, Im speaking in Uwuish Language!'));
